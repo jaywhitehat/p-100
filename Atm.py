deb1178c2ff6a7f4d3793949bb6bd22e02ea5b8c
jaywhitehat <80339442+jaywhitehat@users.noreply.github.com>
2022-01-09 14:19:28 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,10 @@
+class Atm(object):
+    def __init__(self,cardNo,pinNo):
+        self.cardNo=cardNo,
+        self.pinNo=pinNo,
+    
+    def start(self):
+        print("car started")
+        
+
+    iciciBank = object("73737393378371","2849")
